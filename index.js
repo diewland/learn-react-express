761d4735395fd3090994b81a49033326af33c2ce
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-08-23 17:58:36 +0700

    next: jsx

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ class App extends Component {
 
 const root = document.querySelector('#app')
 ReactDOM.render(<App />, root)
+
+// *** test only ***
+const fetchData = async () => {
+  return fetch('https://randomuser.me/api/')
+}
+
+const printData = async () => {
+  try {
+    const data = await fetchData()
+    const json = await data.json()
+    console.log(json)
+  } catch(e) {
+    console.error("Problem", e)
+  }
+}
+
+printData()
